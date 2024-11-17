c5fcf9156d6694be568bd0da11e71dfe47aa6272
David T. Pocock <davidtpocock@gmail.com>
2024-11-17 16:39:30 +0000

    web_app: align with firmware notification changes

--- web_app/app.js
+++ web_app/app.js
@@ -1,7 +1,7 @@
 import { addTimestampToMessage } from './utils.js';
 
 var statusText = document.querySelector('#statusText');
-var clickCounter = 0;
+var clickCounter = -1;
 
 // App-specific constants
 const deviceNamePrefix = 'Clicker'
@@ -10,40 +10,37 @@ const txCharacteristicUuid = 'a4de0202-a156-493c-83d8-845c40da5203';
 
 
 statusText.addEventListener('click', function () {
-    bluetoothStreamService.connect(deviceNamePrefix, serviceUuid)
-        .then(() => {
-            // Add event listener for device disconnection
-            bluetoothStreamService.device.addEventListener('gattserverdisconnected', onDisconnected);
+	bluetoothStreamService.connect(deviceNamePrefix, serviceUuid)
+		.then(() => {
+			// Add event listener for device disconnection
+			bluetoothStreamService.device.addEventListener('gattserverdisconnected', onDisconnected);
 
-            // Reset click counter
-            statusText.textContent = 'Clicks: 0';
-
-            // Subscribe to notifications
-            return bluetoothStreamService.subscribeToNotifications(serviceUuid, txCharacteristicUuid);
-        })
-        .then(characteristic => {
-            handleNotification(characteristic);
-        })
-        .catch(error => {
-            if (error.name !== 'NotFoundError') {
-                statusText.textContent = `Error: ${error}`;
-            }
-        });
+			// Subscribe to notifications
+			return bluetoothStreamService.subscribeToNotifications(serviceUuid, txCharacteristicUuid);
+		})
+		.then(characteristic => {
+			handleNotification(characteristic);
+		})
+		.catch(error => {
+			if (error.name !== 'NotFoundError') {
+				statusText.textContent = `Error: ${error}`;
+			}
+		});
 });
 
 function handleNotification(notification) {
-    notification.addEventListener('characteristicvaluechanged', event => {
-        var notification = bluetoothStreamService.parseNotification(event.target.value);
-        increaseClickCounter(notification);
-    });
+	notification.addEventListener('characteristicvaluechanged', event => {
+		var notification = bluetoothStreamService.parseNotification(event.target.value);
+		increaseClickCounter(notification);
+	});
 }
 
 function onDisconnected(event) {
-    statusText.textContent = 'Disconnected';
-    console.log(addTimestampToMessage('Disconnected from device'));
+	statusText.textContent = 'Disconnected';
+	console.log(addTimestampToMessage('Disconnected from device'));
 }
 
 function increaseClickCounter(temperature) {
-    clickCounter += 1;
-    statusText.innerHTML = 'Clicks: ' + clickCounter.toString() + '<br>Temperature: ' + temperature.toString() + ' °C';
+	clickCounter += 1;
+	statusText.innerHTML = 'Clicks: ' + clickCounter.toString() + '<br>Temperature: ' + temperature.toString() + ' °C';
 }
